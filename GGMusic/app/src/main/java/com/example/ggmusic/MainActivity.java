6dc679a0838abb5a9644da26dce44d0f2a795b81
chenyi <71563698@qq.com>
2023-07-05 17:34:20 +0800

    added music player

--- GGMusic/app/src/main/java/com/example/ggmusic/MainActivity.java
+++ GGMusic/app/src/main/java/com/example/ggmusic/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ggmusic;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,13 +10,32 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BottomNavigationView navigation;
+    private TextView tvBottomTitle;
+    private TextView tvBottomArtist;
+    private ImageView ivAlbumThumbnail;
+    private View ivPlay;
+
+    private MediaPlayer mMediaPlayer = null;
     private final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -46,6 +67,24 @@ public class MainActivity extends AppCompatActivity {
         } else {
             initPlaylist();
         }
+
+
+        navigation = findViewById(R.id.navigation);
+        LayoutInflater.from(MainActivity.this)
+                .inflate(R.layout.bottom_media_toolbar,
+                        navigation,
+                        true);
+
+        ivPlay = navigation.findViewById(R.id.iv_play);
+        tvBottomTitle = navigation.findViewById(R.id.tv_bottom_title);
+        tvBottomArtist = navigation.findViewById(R.id.tv_bottom_artist);
+        ivAlbumThumbnail = navigation.findViewById(R.id.iv_thumbnail);
+
+        if (ivPlay != null) {
+            ivPlay.setOnClickListener(MainActivity.this);
+        }
+
+        navigation.setVisibility(View.GONE);
     }
 
     private void initPlaylist() {
@@ -67,7 +106,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
+                                           String[] permissions,
+                                           int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_EXTERNAL_STORAGE:
@@ -79,5 +119,75 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+        super.onStop();
+    }
+
+    private ListView.OnItemClickListener itemClickListener
+            = new ListView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView,
+                                View view, int i, long l) {
+            Cursor cursor = mCursorAdapter.getCursor();
+            if (cursor != null && cursor.moveToPosition(i)) {
+
+                int titleIndex = cursor.getColumnIndex(
+                        MediaStore.Audio.Media.TITLE);
+                int artistIndex = cursor.getColumnIndex(
+                        MediaStore.Audio.Media.ARTIST);
+                int albumIdIndex = cursor.getColumnIndex(
+                        MediaStore.Audio.Media.ALBUM_ID);
+                int dataIndex = cursor.getColumnIndex(
+                        MediaStore.Audio.Media.DATA);
+
+
+
+                String title = cursor.getString(titleIndex);
+                String artist = cursor.getString(artistIndex);
+                Long albumId = cursor.getLong(albumIdIndex);
+                String data = cursor.getString(dataIndex);
+
+
+                Uri dataUri = Uri.parse(data);
+
+                if (mMediaPlayer != null) {
+                    try {
+                        mMediaPlayer.reset();
+                        mMediaPlayer.setDataSource(
+                                MainActivity.this, dataUri);
+                        mMediaPlayer.prepare();
+                        mMediaPlayer.start();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
